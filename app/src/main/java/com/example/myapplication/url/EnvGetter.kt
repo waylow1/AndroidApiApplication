09581ef33a3e66f534f26d6e109a88b749978890
Maxence <levesqu221@campus.unicaen.fr>
2024-02-22 17:30:28 +0100

    build: env getter

--- app/src/main/java/com/example/myapplication/url/EnvGetter.kt
+++ app/src/main/java/com/example/myapplication/url/EnvGetter.kt
@@ -1,4 +1,24 @@
 package com.example.myapplication.url
 
+import android.content.Context
+
 class EnvGetter {
+
+    fun getEnv(context: Context): String {
+        val assetManager = context.assets
+        var url="";
+        try {
+            val inputStream = assetManager.open(".env")
+            val bufferedReader = inputStream.bufferedReader()
+            bufferedReader.useLines { lines ->
+                lines.forEach {
+                    url= it.split("=")[1]
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return url;
+    }
+
 }
